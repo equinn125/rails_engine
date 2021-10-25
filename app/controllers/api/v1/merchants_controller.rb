0f097f396ab848e585215380d7165fef91a08092
Erin Quinn <equinn125@gmail.com>
2021-10-24 20:25:49 -0600

    complete merchant show endpoint

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,8 +1,16 @@
 class Api::V1::MerchantsController < ApplicationController
-
   def index
     merchants = Merchant.limit(limit_amount).offset(limit_amount * page_offset)
     render json: MerchantSerializer.new(merchants)
+  end
+
+  def show
+
+    if merchant = Merchant.find_by(id: params[:id])
+    render json: MerchantSerializer.new(merchant)
+  else
+    render json: '{"error": "not_found"}', status: 404
+  end
   end
 
   private
