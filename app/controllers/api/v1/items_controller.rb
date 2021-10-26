f528fe980bcd638d078403565886d1d2f2ad2351
Erin Quinn <equinn125@gmail.com>
2021-10-25 21:46:33 -0600

    impl create item endpoint

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -11,4 +11,18 @@ class Api::V1::ItemsController < ApplicationController
     render json: '{"error": "not_found"}', status: 404
     end
   end
+
+  def create
+  item = Item.new(item_params)
+  if item.save
+    render json: ItemSerializer.new(item), status: 201
+  else
+    render json: '{"error": "bad_request"}', status: 400
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     name { Faker::Commerce.product_name }
     description { Faker::GreekPhilosophers.quote }
     unit_price { Faker::Number.decimal(l_digits: 3, r_digits: 2)}
-    merchant
+     association :merchant
   end
 end
