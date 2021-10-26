2e099d10fd14144547cfd92336733ad2f0345105
Erin Quinn <equinn125@gmail.com>
2021-10-25 19:29:05 -0600

    complete item show endpoint

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,4 +3,12 @@ class Api::V1::ItemsController < ApplicationController
     items = Item.limit(limit_amount).offset(limit_amount * page_offset)
     render json: ItemSerializer.new(items)
   end
+
+  def show
+  if   item = Item.find_by(id: params[:id])
+    render json: ItemSerializer.new(item)
+  else
+    render json: '{"error": "not_found"}', status: 404
+    end
+  end
 end
