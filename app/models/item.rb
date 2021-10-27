6f16a8aca5dca616b0db923ccb2037dfc18b61b7
Erin Quinn <equinn125@gmail.com>
2021-10-27 00:39:32 -0600

    complete find item fucntionality

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,29 @@ class Item < ApplicationRecord
   validates_presence_of :description, presence: true
   validates_presence_of :unit_price, presence: true
   validates_presence_of :merchant_id, presence: true
+
+  def self.find_all_prices(min, max)
+    if min && !max
+      where("unit_price >= ?", min)
+      .order(:name)
+    elsif max && !min
+      where("unit_price <= ?", max)
+      .order(:name)
+    else
+      where('unit_price < ? AND unit_price > ?', max, min)
+      .order(:name)
+    end
+  end
+
+  def self.find_item_name(params)
+    where("name ILIKE ?", "%#{params}%")
+  end
+  #
+  # def self.find_by_min(price_params)
+  #   where("unit_price >= ?", price_params[:min_params].to_f)
+  # end
+  #
+  # def self.find_by_max(price_params)
+  #   where("unit_price <= ?", price_params[:max_params].to_f)
+  # end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,4 +4,40 @@ RSpec.describe Item, type: :model do
   describe 'relationships' do
      it { should belong_to(:merchant) }
   end
+
+  it 'finds a item by name' do
+    merchant = create(:merchant)
+    item1 = create(:item, name: 'shampoo', description: 'shampoo for curly hair',unit_price: 4.99, merchant_id: merchant.id)
+    item2 =  create(:item, name: 'conditioner', description: 'conditioner for curly hair',unit_price: 5.99, merchant_id: merchant.id)
+    item3 =  create(:item, name: 'hair oil', description: 'hair oil for curly hair',unit_price: 6.99, merchant_id: merchant.id)
+
+    expect(Item.find_item_name('sham')).to eq([item1])
+  end
+
+  xit 'finds item by minimum price' do
+    merchant = create(:merchant)
+    item1 = create(:item, name: 'shampoo', description: 'shampoo for curly hair',unit_price: 4, merchant_id: merchant.id)
+    item2 =  create(:item, name: 'conditioner', description: 'conditioner for curly hair',unit_price: 5, merchant_id: merchant.id)
+    item3 =  create(:item, name: 'hair oil', description: 'hair oil for curly hair',unit_price: 6, merchant_id: merchant.id)
+    params = { min_params: 3}
+    expect(Item.find_by_min(params)).to eq([item1, item2, item3])
+  end
+
+  xit 'finds item by maximum price' do
+    merchant = create(:merchant)
+    item1 = create(:item, name: 'shampoo', description: 'shampoo for curly hair',unit_price: 4, merchant_id: merchant.id)
+    item2 =  create(:item, name: 'conditioner', description: 'conditioner for curly hair',unit_price: 5, merchant_id: merchant.id)
+    item3 =  create(:item, name: 'hair oil', description: 'hair oil for curly hair',unit_price: 6, merchant_id: merchant.id)
+    params = { max_params: 7}
+    expect(Item.find_by_max(params)).to eq([item1,item2,item3])
+  end
+
+  xit 'finds item within min and maximum price' do
+    merchant = create(:merchant)
+    item1 = create(:item, name: 'shampoo', description: 'shampoo for curly hair',unit_price: 4, merchant_id: merchant.id)
+    item2 =  create(:item, name: 'conditioner', description: 'conditioner for curly hair',unit_price: 5, merchant_id: merchant.id)
+    item3 =  create(:item, name: 'hair oil', description: 'hair oil for curly hair',unit_price: 6, merchant_id: merchant.id)
+    params = {min_params:4, max_params: 6}
+    expect(Item.find_all_prices(params)).to eq([item1,item2,item3])
+  end
 end
